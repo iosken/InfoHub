350572f25b8a6f010ac3325705037aafcf90af0a
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-08-29 10:02:41 +0500

    base setup with InfoHubViewController

---
+++ InfoHub/InfoHubViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InfoHubViewController.swift
+//  InfoHub
+//
+//  Created by Yuri Volegov on 29.08.2023.
+//
+
+import UIKit
+
+final class InfoHubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
